42b22cceb960dadb4ed2d49702acd64a08add3ed
NishantSingh02 <nst7372@gmail.com>
2023-05-05 02:26:44 +0530

    updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-});
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_PROD_URL, {
+      useNewUrlParser: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB: ", err);
+  }
+}
+
+connectToDatabase();
