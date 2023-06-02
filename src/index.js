d9918e4813058a084881dc5bcc78cb161c489ece
NishantSingh02 <nst7372@gmail.com>
2023-06-02 17:17:38 +0530

    heartbeat point added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require("./db/mongoose");
 const userRouter = require("./routers/user");
 const articleRouter = require("./routers/article");
 const sharingRouter = require("./routers/sharing");
+const http = require("http");
 
 const cors = require("cors");
 
@@ -19,3 +20,23 @@ app.use(sharingRouter); // regstering sharing router
 app.listen(port, () => {
   console.log("Server is up on the port " + port);
 });
+
+// Send periodic requests to the dummy endpoint every 5 minutes
+setInterval(() => {
+  const options = {
+    hostname: "localhost", // Update with your server's hostname or IP address
+    port: port, // Update with your server's port number
+    path: "/dummy",
+    method: "GET",
+  };
+
+  const req = http.request(options, (res) => {
+    console.log(`server is live. Response status: ${res.statusCode}`);
+  });
+
+  req.on("error", (error) => {
+    console.error("Error sending heartbeat request:", error);
+  });
+
+  req.end();
+}, 15 * 60 * 1000); // Send the dummy request every 15 minutes
