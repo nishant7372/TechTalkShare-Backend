3a95d63008f0e5d430a2d217b761d309a92b0230
NishantSingh02 <nst7372@gmail.com>
2023-06-06 18:43:25 +0530

    download route added

---
+++ src/routers/download.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const auth = require("../middleware/auth");
+const Download = require("../models/download");
+
+const router = new express.Router();
+
+router.get("/downloads", auth, async (req, res) => {
+  try {
+    const downloads = await Download.find({
+      owner: req.user._id,
+    });
+    res.send(downloads);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+router.delete("/downloads/:id", auth, async (req, res) => {
+  try {
+    const download = await Download.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+
+    if (!download) {
+      return res.status(404).send();
+    }
+    res.send(download);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
