4f707db4a6ed6e4131db4173fc0c1b4c547662d6
NishantSingh02 <nst7372@gmail.com>
2023-07-04 19:48:59 +0530

    added

---
+++ src/routers/message.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const auth = require("../middleware/auth");
+const Message = require("./../models/message");
+const User = require("../models/user");
+
+const router = new express.Router();
+
+router.get("/messages/:userName", auth, async (req, res) => {
+  const { userName } = req.params;
+  try {
+    const receiver = await User.findOne({ userName });
+    if (!receiver) {
+      return res.status(404).send({ message: "User Not Found" });
+    }
+
+    const messagesSentByReceiver = await Message.find({
+      sender: receiver._id, // Get messages sent by the receiver to the authenticated user
+      reciever: req.user._id, // The authenticated user is the receiver in this case
+    });
+
+    const messagesSentByUser = await Message.find({
+      sender: req.user._id, // The authenticated user is the sender in this case
+      reciever: receiver._id, // Get messages sent by the authenticated user to the receiver
+    });
+
+    const allMessages = [...messagesSentByReceiver, ...messagesSentByUser].sort(
+      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+    );
+
+    res.send(allMessages);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+// router.post("/message", auth, async (req, res) => {
+//   const { content, userName } = req.body;
+//   try {
+//     const reciever = await User.findOne({ userName });
+//     if (!reciever) {
+//       return res.status(404).send({ message: "User Not Found" });
+//     }
+//     const message = new Message({
+//       content,
+//       sender: req.user._id,
+//       reciever: reciever._id,
+//     });
+//     await message.save();
+//     res.status(201).send();
+//   } catch (error) {
+//     return res.status(400).send({ message: error.message });
+//   }
+// });
+
+router.patch("/message/:id", auth, async (req, res) => {
+  const { id } = req.params;
+  try {
+    // checking if the updates are allowed
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["content"];
+
+    const isValidUpdate = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
+
+    if (!isValidUpdate) {
+      return res.status(400).send({ message: "Invalid Updates!" });
+    }
+    const message = await Message.findOne({ _id: id, sender: req.user._id });
+    if (!message) {
+      return res.status(404).send({ message: "Message Not Found" });
+    }
+    updates.forEach((update) => (message[update] = req.body[update]));
+    await message.save();
+    res.status(200).send();
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
+  }
+});
+
+module.exports = router;
