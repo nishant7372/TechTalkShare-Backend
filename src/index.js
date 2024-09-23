7a2e469d1b6f041910919ccde442992f1b60d4e3
nishant-juno <nishant@onjuno.com>
2024-09-23 13:34:12 +0530

    all scraping operations remove

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
-const { router: articleRouter, activeDownloads } = require("./routers/article");
+const articleRouter = require("./routers/article");
 const sharingRouter = require("./routers/sharing");
 const downloadRouter = require("./routers/download");
 const messageRouter = require("./routers/message");
@@ -25,8 +25,8 @@ app.use(cors(corsOptions));
 
 app.use(express.json()); // parse incoming json to object for accessing it in request handlers
 app.use(userRouter); // registering user router
-app.use(articleRouter); // regstering article router
-app.use(sharingRouter); // regstering sharing router
+app.use(articleRouter); // registering article router
+app.use(sharingRouter); // registering sharing router
 app.use(downloadRouter); // registering download Router
 app.use(messageRouter); // registering message Router
 app.use(storeRouter); // registering store Router
@@ -68,12 +68,8 @@ io.on("connection", (socket) => {
     console.log("Online Chat Clients: ", chatClients.keys());
   });
 
-  socket.on("downloadStatus", (getActiveDownloads) => {
-    getActiveDownloads(activeDownloads);
-  });
-
   // when a sender sends a message
-  // - Server sends it to reciever (when online)
+  // - Server sends it to receiver (when online)
   // - Server save the message in database as well
   socket.on("new_message", async (newMessage) => {
     const { sender, content, reciever } = newMessage;
